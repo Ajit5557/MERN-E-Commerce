a371b955ef603a45d4ac0b2ee525b58bf92b2345
Ajit5557 <your_github_email@example.com>
2025-10-22 18:35:39 +0530

    fix: update vercel config for frontend build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,6 +11,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  base:'/',
   plugins: [
     react(),
     tailwindcss(),
